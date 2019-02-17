40c0a20af6d0339958f01f1f8ab1251baddd2a8f
H. M. Shahriar <mshahriar.h@gmail.com>
2019-02-17 20:01:12 +0600

    Added dashboard header ripple color changed

--- app/src/main/java/com/shahriar/surahshikkha/UI/DashboardActivity.java
+++ app/src/main/java/com/shahriar/surahshikkha/UI/DashboardActivity.java
@@ -7,6 +7,7 @@ import android.content.res.Configuration;
 import android.database.DatabaseErrorHandler;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -188,22 +189,6 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     }
 
     void initComponent(Context context) {
-
-        String yourText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                "Ut volutpat interdum interdum. Nulla laoreet lacus diam, vitae " +
-                "sodales sapien commodo faucibus. Vestibulum et feugiat enim. Donec " +
-                "semper mi et euismod tempor. Sed sodales eleifend mi id varius. Nam " +
-                "et ornare enim, sit amet gravida sapien. Quisque gravida et enim vel " +
-                "volutpat. Vivamus egestas ut felis a blandit. Vivamus fringilla " +
-                "dignissim mollis. Maecenas imperdiet interdum hendrerit. Aliquam" +
-                " dictum hendrerit ultrices. Ut vitae vestibulum dolor. Donec auctor ante" +
-                " eget libero molestie porta. Nam tempor fringilla ultricies. Nam sem " +
-                "lectus, feugiat eget ullamcorper vitae, ornare et sem. Fusce dapibus ipsum" +
-                " sed laoreet suscipit. ";
-
-        ExpandableTextView expandableTextView = (ExpandableTextView) findViewById(R.id.headerText);
-        expandableTextView.setText(yourText);
-        expandableTextView.setBackgroundColor(getResources().getColor(R.color.window_background));
         Log.d(getClass().getSimpleName(), "initComponent");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -215,10 +200,11 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
         mediaHandler = new Handler();
 
-        sortTypeTextHeaderView = (TextView) findViewById(R.id.SortTextHeader);
-        sortTypeTextHeaderView.setTypeface(typeface);
-        sortTypeTextView = (TextView) findViewById(R.id.SortText);
-        sortTypeTextView.setTypeface(typeface);
+//        sortTypeTextHeaderView = (TextView) findViewById(R.id.SortTextHeader);
+//        sortTypeTextHeaderView.setTypeface(typeface);
+//        sortTypeTextView = (TextView) findViewById(R.id.SortText);
+//        sortTypeTextView.setTypeface(typeface);
+
         Log.d(getClass().getSimpleName(), "setHomeAsUpIndicator");
         surahListView = (RecyclerView) findViewById(R.id.surahList);
         surahListView.setHasFixedSize(true);
@@ -483,7 +469,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
             mAdapter.notifyDataSetChanged();
             scrollListToPosition(0);
         }
-        setSortText(type, localizedContext);
+        //setSortText(type, localizedContext);
     }
 
     /*
